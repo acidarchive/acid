ac8aeb1e7852652346718c934d0dcdad60c76dee
db303 <dev@acidarchive.com>
2025-05-17 22:06:11 +0100

    feat: deploy to prod

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use acid::startup::Application;
 use acid::telemetry::{get_subscriber, init_subscriber};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenvy::dotenv()?;
+    // dotenvy::dotenv()?;
 
     let subscriber = get_subscriber("api".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
